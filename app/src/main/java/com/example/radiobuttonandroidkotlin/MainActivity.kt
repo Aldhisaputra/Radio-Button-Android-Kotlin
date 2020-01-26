17bf65b72e4ed6dcd4e040f02395c8020a672ae6
Aldhi Saputra <aldhisaputra795@gmail.com>
2020-01-26 19:55:58 +0700

    RadioButtonAndroidKotlin

--- app/src/main/java/com/example/radiobuttonandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/example/radiobuttonandroidkotlin/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.radiobuttonandroidkotlin
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        radio_group.setOnCheckedChangeListener(
+
+            RadioGroup.OnCheckedChangeListener { group, checkedId ->
+                val radio: RadioButton = findViewById(checkedId)
+
+                Toast.makeText(applicationContext,"${radio.text}",Toast.LENGTH_SHORT).show()
+
+                var male = "Laki-Laki"
+
+                result.text = "$male"
+
+
+
+
+
+            })
+
+
     }
+
+
+
+
 }
